e287eed32f3c068972500d4fcc2cb2e33b7cb9d0
ctr13e <christian.rayjr@gmail.com>
2016-04-14 19:36:52 +0000

    Lesson 7 final commit

---
+++ Lesson7_Assignment/drill4.js
@@ -0,0 +1,35 @@
+/*
+ Project Name:  Recipe Display Application
+      
+      Author: Christian Ray
+      
+      Date:   4/14/2016
+
+      Filename: script.js
+*/
+
+// displays the next element after the current target 
+function display (event) {
+
+
+    $ (event.currentTarget).next().fadeIn("slow");
+    
+} //end of display
+
+//attach event listener to h3 elements to invoke display function when clicked
+//$ ("h3").click(display);
+
+//displays and animates the next elemtent after the current target
+function display2(event){
+    $(event.currentTarget).next().animate({height: 'toggle'}, "slow");
+}//end of display2
+
+//attach event listener to h3 elements to invoke display function when clicked
+$("h3").click(display2);
+
+//change the background color h3 element when mouse hovers over it
+$("h3").hover (function(){
+    $(this).css("background-color", "yellow");
+}, function(){
+    $(this).css("background-color", "pink");
+});
